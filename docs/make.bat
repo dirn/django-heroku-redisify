3e6764dbf0374c93b3a34a276b11c414e1c7789b
Andy Dirnberger <dirn@dirnonline.com>
2012-07-26 17:24:08 -0400

    Clean up Sphinx makefiles
    When I started the docs I did so by copying and pasting them from another project (as previously noted in 517e5ce). It looks like I failed to update the makefiles as well.

--- docs/make.bat
+++ docs/make.bat
@@ -99,9 +99,9 @@ if "%1" == "qthelp" (
 	echo.
 	echo.Build finished; now you can run "qcollectiongenerator" with the ^
 .qhcp project file in %BUILDDIR%/qthelp, like this:
-	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\Whenpy.qhcp
+	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\django-heroku-redisify.qhcp
 	echo.To view the help file:
-	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\Whenpy.ghc
+	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\django-heroku-redisify.ghc
 	goto end
 )
 
